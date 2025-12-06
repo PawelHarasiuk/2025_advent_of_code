b8aeb42a7234c3a32e9bdf0868ea9129bf77eca0
PawelHarasiuk <pharasiuk01@gmail.com>
2025-12-06 19:57:01 +0100

    updated ex1

--- src/Ex1.java
+++ src/Ex1.java
@@ -5,33 +5,28 @@ import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        int curr = 50;
-        int count = 0;
-
         try {
-            InputStream inputStream = new FileInputStream("./src/ex1_input.txt");
+            InputStream inputStream = new FileInputStream("./src/ex2_input.txt");
             Scanner scanner = new Scanner(inputStream);
-
+            int curr = 50;
+            int count = 0;
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
-                char rotation = line.charAt(0);
-                int value = Integer.parseInt(line.substring(1)) % 100;
+                char code = line.charAt(0);
+                int rotation = Integer.parseInt(line.substring(1));
 
-                if (rotation == 'R') {
-                    curr += value;
+                while (rotation > 0) {
+                    if (code == 'R') curr++;
+                    if (code == 'L') curr--;
+                    if (curr > 99) curr = 0;
+                    if (curr < 0) curr = 99;
+                    rotation--;
                 }
-                if (rotation == 'L') {
-                    curr -= value;
-                }
-
-                if (curr > 99) curr = curr - 100;
-                if (curr < 0) curr = curr + 100;
                 if (curr == 0) count++;
             }
+            System.out.println(count);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println(count);
     }
 }
